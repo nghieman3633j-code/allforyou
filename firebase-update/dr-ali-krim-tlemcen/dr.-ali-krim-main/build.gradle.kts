2080ca96a29abbe5b08f18420d4d95688501cd26
nghieman3633j-code <nghieman3633j@gmail.com>
2026-06-16 01:36:26 +0100

    Update build.gradle.kts

--- firebase-update/dr-ali-krim-tlemcen/dr.-ali-krim-main/build.gradle.kts
+++ firebase-update/dr-ali-krim-tlemcen/dr.-ali-krim-main/build.gradle.kts
@@ -1,13 +1,11 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+plugins {
+    id("com.android.application") version "9.1.1" apply false
+    id("com.android.library") version "9.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+// إعدادات بناء المشروع الفرعي الافتراضية
+subprojects {
     repositories {
         google()
         mavenCentral()
