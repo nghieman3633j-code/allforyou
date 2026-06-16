168dabc42a465b2bb4c1a40f31a1976f7c632d1f
nghieman3633j-code <nghieman3633j@gmail.com>
2026-06-16 01:40:55 +0100

    Update build.gradle.kts

--- firebase-update/dr-ali-krim-tlemcen/dr.-ali-krim-main/build.gradle.kts
+++ firebase-update/dr-ali-krim-tlemcen/dr.-ali-krim-main/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
-    id("com.android.application") version "9.1.1" apply false
-    id("com.android.library") version "9.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 // إعدادات بناء المشروع الفرعي الافتراضية
